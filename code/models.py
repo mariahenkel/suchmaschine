fa310cd00e8d9bb6a9ee1f532ba62608e2b61aa3
jubar103 <julia.barth@uni-duesseldorf.de>
2015-04-30 18:04:36 +0200

    Database model
    English version

--- code/models.py
+++ code/models.py
@@ -8,70 +8,70 @@ from sqlalchemy.orm import sessionmaker
 Base = declarative_base()
 	
 """
- Klasse Dokument
+ Class Document
 
 """
 
-class Dokument(Base):
-	__tablename__ = 'dokument'
-	dokumentid = Column(Integer, primary_key=True)
+class Document(Base):
+	__tablename__ = 'document'
+	documentid = Column(Integer, primary_key=True)
 	url = Column(String(500))
-	titel = Column(String(100))
+	title = Column(String(100))
 	ranking = Column(Float)
-	quelltext = Column(Text)
+	sourcetext = Column(Text)
 	thumbnail = Column(BLOB)
 	gifs = Column(Integer)
-	musikhintergrund = Column(Boolean)
-	musikloop= Column(Boolean)
-	schriftart_vorhanden = Column(Boolean)
-	schriftart_anzahl = Column(Integer)
+	backgroundmusic = Column(Boolean)
+	musicloop= Column(Boolean)
+	font_existing = Column(Boolean)
+	font_number = Column(Integer)
 	schriftformat = Column(Boolean)
-	seitenstruktur = Column(Boolean)
-	farbe = Column(Integer)
+	pagestructure= Column(Boolean)
+	colour = Column(Integer)
 	textanimation = Column(Integer)
-	phrasen = Column(Integer)
-	bild_verzehrt = Column(Integer)
-	fehlerlinks= Column(Integer)
-	besucherzaehler = Column(Boolean)
-	gaestebuch = Column(Boolean)
-	werbung = Column(Integer)
+	phrases = Column(Integer)
+	picture_distorted = Column(Integer)
+	corruptlinks = Column(Integer)
+	hitcounter = Column(Boolean)
+	guestbook = Column(Boolean)
+	advertising = Column(Integer)
 	shoutbox = Column(Boolean)
 	htmlversion = Column(Boolean)
 	w3c = Column(Integer)
 	popup = Column(Boolean)
 	flash = Column(Boolean)
 	overall_score = Column(Float)
-	wortlisten = relationship('Besteht_aus', backref = 'dokument')
+	wordlists = relationship('Consists_of', backref = 'document')
 	
 """
- Klasse Wortliste
+ Class wordlist
 
 """
 	
-class Wortliste(Base):
-	__tablename__ = 'wortliste'
-	wortid = Column(Integer, primary_key = True)
-	wort = Column(String(50))
-	wortstamm = Column(String(50))
-	stoppwort = Column(Boolean)
-	anzahl = Column(Integer)
+class Wordlist(Base):
+	__tablename__ = 'wordlist'
+	wordid = Column(Integer, primary_key = True)
+	word = Column(String(50))
+	stem = Column(String(50))
+	stopword = Column(Boolean)
+	number = Column(Integer)
 	idf = Column(Float)
 
 	
 """
- Relation besteht_aus 	
+ Relation consists_of 	
 
 """
 
-class Besteht_aus(Base):
-	__tablename__ = 'besteht_aus'
-	satznr = Column(Integer)
+class Consists_of(Base):
+	__tablename__ = 'consists_of'
+	sentenceno = Column(Integer)
 	position = Column(Integer)
-	stoppwort = Column(Integer)
+	stopword = Column(Integer)
 	wdf = Column(Float)
-	dokument_dokumentid = Column(Integer, ForeignKey('dokument.dokumentid'), primary_key = True)
-	wortliste_wortid = Column(Integer, ForeignKey('wortliste.wortid'), primary_key = True)
-	worliste = relationship('Wortliste', backref = 'dokument_assocs')
+	document_documentid = Column(Integer, ForeignKey('document.documentid'), primary_key = True)
+	wordlist_wordid = Column(Integer, ForeignKey('wordlist.wordid'), primary_key = True)
+	wordlist = relationship('Wordlist', backref = 'document_assocs')
 	
 if __name__ == "__main__":
 	engine = create_engine('sqlite:///search.db', echo=True)
