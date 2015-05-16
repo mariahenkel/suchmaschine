6c0fbab2878735539522dc03d86e2676b8de8891
M4urice <Maurice.Schleussinger@uni-duesseldorf.de>
2015-05-16 18:07:58 +0200

    Sett some settings for broad crawls as suggested in the docs of scrapy

--- code/config.py
+++ code/config.py
@@ -10,14 +10,17 @@ SPIDER_MODULES = ["basic_crawler.spiders"]
 NEWSPIDER_MODULE = "basic_crawler.spiders"
 ITEM_PIPELINES = {"basic_crawler.pipelines.InvertaPipeline": 1}
 
-# Settings to speed things up a bit
+# Settings to speed things up a bit (also see
+# http://doc.scrapy.org/en/0.24/topics/broad-crawls.html)
 AUTOTHROTTLE_ENABLED = True
 REDIRECT_MAX_TIMES = 3
-CONCURRENT_REQUESTS = 20
+CONCURRENT_REQUESTS = 100
 RETRY_ENABLED = False
 DOWNLOAD_TIMEOUT = 30
 DEPTH_LIMIT = 1
+COOKIES_ENABLED = False
 
+AJAXCRAWL_ENABLED = True
 # Less verbose output and use of a logfile
 LOG_LEVEL = "INFO"
 LOG_FILE = path.join(pardir, "logs", "basic_crawler.log")
