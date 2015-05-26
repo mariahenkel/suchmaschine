0f21b0b2844226ef3ddb31a75748c54c363d4b5c
ayilh <aylin.ilhan@uni-duesseldorf.de>
2015-05-26 12:43:14 +0200

    Update indexer.py

--- code/indexer.py
+++ code/indexer.py
@@ -6,23 +6,37 @@ sys.setdefaultencoding("utf-8")
 from BeautifulSoup import BeautifulSoup
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
+from bs4 import BeautifulSoup, Comment
 
+
+
+#llllllll
 soup = BeautifulSoup(open("index.html"))
+for child in soup.body:
+    if isinstance(child,Comment):
+        child.extract()
+
+#soup.encode('utf-8')
 #invalid_tags = ['b','div', 'i', 'p', 'ul', 'li', 'ol', 'h1', 'h2', 'h3', 'pre', 'code','a', 'blockquote', 'strong', 'em']
+
+#BeautifulSoup(soupex, convertEntities=BeautifulSoup.HTML_ENTITIES)
+#soup.html.replaceWith(u'<html>'+unicode('utf-8')+u'</html>')
+
+#soups = BeautifulSoup.(soup.decode('utf-8','ignore'))
 
 
 #test=soup.find('body')					
 #for tag in invalid_tags: 
  #   for match in test.findAll(tag):
   #      match.replaceWithChildren()
-
-
-
 body_texts = soup.body(text=True)
+
+
 
 liste=[]
 for element in body_texts:
 	liste.append(element)
+
 
 
 #for element in liste:
@@ -40,6 +54,7 @@ with open("extrahier.txt", "a") as datei:
 #---------------------------------------------------------
 
 #Worte extrahieren 
+liste=[]
 with open("extrahier.txt", "r") as datei:
 	worteextrahieren=datei.read().split()
 	#print worteextrahieren
