72186ac692a9101ae0a6bd35065eecd4c2ebe7f8
Maria <mahen2@users.noreply.github.com>
2015-05-26 09:59:55 -0400

    Indexer vereinfacht

--- code/indexer.py
+++ code/indexer.py
@@ -1,60 +1,42 @@
 # -*- coding: utf-8 -*-
-# 
-#import csv
-# Encoding für die Datei
+
 import sys
-reload(sys)
-sys.setdefaultencoding("utf-8")
+
 # Die folgende Zeile führt unter Linux zu einer Fehlermeldung. Bitte ggf. (ent)kommentieren.
 #from BeautifulSoup import BeautifulSoup
-# Import von BS und Comment 
-from bs4 import BeautifulSoup, Comment
-# Stoppwortliste
-from nltk.corpus import stopwords
-# Stemmer
-from nltk.stem import PorterStemmer
+
+from bs4 import BeautifulSoup, Comment # BeautifulSoup und Comment
+from nltk.corpus import stopwords # Stoppwortliste
+from nltk.stem import PorterStemmer # Stemmer
+
+# Encoding für die Datei
+reload(sys)
+sys.setdefaultencoding("utf-8")
 
 # Öffnen der Datei/Seite
 soup = BeautifulSoup(open("index.html"))
+
 # Entfernen der Kommentare
 for child in soup.body:
     if isinstance(child,Comment):
         child.extract()
 
-# Übergabe des Bodys
-body_texts = soup.body(text=True)
+# TO DO: Es müssen noch Satzzeichen und andere Störfaktoren entfert werden!
 
-# Listenerstellung
-liste=[]
-for element in body_texts:
-	liste.append(element)
-
-# Liste entfernen???
-reine_textdatei=" ".join(liste).lower()
-
-# Datei öffnen und mit Daten füllen
-with open("extrahier.txt", "a") as datei:
-	for element in reine_textdatei:
-		datei.write(element)
+# Übergabe des Textes und Erstellung einer Wortliste zur Weiterverarbeitung
+body_text = soup.body.getText()
+word_list = body_text.lower().split( ) # Hier geht die Reihenfolge verloren!!!
 
 #---------------------------------------------------------  
 # Ab hier Stemming / Stoppworterkennung
 #---------------------------------------------------------
 
-#Worte extrahieren 
-liste=[]
-with open("extrahier.txt", "r") as datei:
-	worteextrahieren=datei.read().split()
-	#print worteextrahieren
-
-
-#-----------------------------------------------------------
 #Anzahl der Worte, Stoppwortmarkierung und gestemmtes Wort werden angezeigt
 
 stop=stopwords.words('english')
 dict={}
 
-for element in worteextrahieren:
+for element in word_list:
 	try:
 		if element not in dict.keys():
 			stemmer=PorterStemmer()
@@ -65,7 +47,7 @@ for element in worteextrahieren:
 			else:
 				dict[element]=[1,False, gestemmtes_wort]
 		else:
-			haufigkeit=worteextrahieren.count(element)
+			haufigkeit=word_list.count(element)
 			if element in stop:
 				dict[element]=[haufigkeit, True, gestemmtes_wort]
 			else:
