cbd9c99169de22bea80fe725f10d5ea38a232344
ayilh <aylin.ilhan@uni-duesseldorf.de>
2015-05-26 10:18:44 +0200

    Update indexer.py
    verbesserte Version

--- code/indexer.py
+++ code/indexer.py
@@ -5,27 +5,28 @@ from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 
 soup = BeautifulSoup(open("index.html"))
-invalid_tags = ['b','div', 'i', 'p', 'ul', 'li', 'ol', 'h1', 'h2', 'h3', 'pre', 'code','a', 'blockquote', 'strong', 'em']
+#invalid_tags = ['b','div', 'i', 'p', 'ul', 'li', 'ol', 'h1', 'h2', 'h3', 'pre', 'code','a', 'blockquote', 'strong', 'em']
 
 
-test=soup.find('body')					#momentanes Problem: Wenn der Code keine Tags hat, wird der Inhalt nicht übernommen
-for tag in invalid_tags: 
-    for match in test.findAll(tag):
-        match.replaceWithChildren()
+#test=soup.find('body')					
+#for tag in invalid_tags: 
+ #   for match in test.findAll(tag):
+  #      match.replaceWithChildren()
 
-body=test
 
+
+body_texts = soup.body(text=True)
 
 liste=[]
-for element in body:
+for element in body_texts:
 	liste.append(element)
 
 
-for element in liste:
-	if element =="<body>" or "</body>":
-		liste.remove(element)
-	else:
-		pass
+#for element in liste:
+#	if element =="<body>" or "</body>":
+#		liste.remove(element)
+#	else:
+#		pass
 
 reine_textdatei=" ".join(liste).lower()
 
