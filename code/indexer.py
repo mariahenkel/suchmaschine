e74a4fb6bf3d85897415806926c93d237c0e2a5f
Maria <mahen2@users.noreply.github.com>
2015-05-28 14:09:30 -0400

    Indexer saves Words in Database
    IDF is not being calculated. Meaning/Use of ID should be clarified.

--- code/indexer.py
+++ code/indexer.py
@@ -42,14 +42,19 @@ word_list = body_text.lower().split( )
 
 
 
-#---------------------------------------------------------  
-# Ab hier Stemming / Stoppworterkennung
-#---------------------------------------------------------
+#----------------------------------------------------------------------
+# Ab hier Stemming / Stoppworterkennung / Einspeisung in die Datenbank
+#----------------------------------------------------------------------
 
 #Dokument-ID???, Wort, Wort-Stamm, Stoppwort(0/1), Position, Anzahl (für WDF/IDF)
 
 stop=stopwords.words('english')
-rdict={}
+#rdict={}
+
+#Verbindung herstellen, Tabelle laden
+engine = create_engine(config.DB_URI, echo=True)
+metadata = MetaData(bind=engine)
+wordtable = Table("wordlist", metadata, autoload=True)
 
 word_pos = 0
 stemmer=PorterStemmer()
@@ -62,27 +67,26 @@ for element in word_list:
 			is_stop = 1
 		else:
 			is_stop = 0
-		rdict[word_pos]=[element,word_stem,is_stop,word_count]
+		#rdict[word_pos]=[element,word_stem,is_stop,word_count]
+		#Daten einfügen
+		#Wordlist: id, word, stem, stopword, number, idf
+		wordtable.insert().execute(word=element, stem=word_stem, stopword=is_stop, number=word_pos, idf=word_count)
+		# Position weiterzählen
 		word_pos = word_pos+1
 	except:
 		pass
 
-print rdict
-
-#---------------------------------------------------------  
-# Ab hier Einspeisung in die Datenbank
-#---------------------------------------------------------
-
-#Verbindung herstellen, Tabelle laden
-#Wordlist: id, word, stem, stopword, number, idf
-
-engine = create_engine(config.DB_URI, echo=True)
-metadata = MetaData(bind=engine)
-wordtable = Table("wordlist", metadata, autoload=True)
-
-ins = wordtable.insert()
-new_word = ins.values(word="clowns", stem="clown", stopword=0, number=1, idf=1)
 
 
-conn = engine.connect()
-conn.execute(new_word)
+
+
+
+
+
+
+
+
+
+
+
+
