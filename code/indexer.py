38134cd84588f4eb34f06afd77a3b7e9c33dc300
Maria <mahen2@users.noreply.github.com>
2015-05-29 04:37:47 -0400

    Indexer reads from database
    Testversion that only reads the last result and prints a dictionary instead of saving in the database. Please (un)comment as desired.

--- code/indexer.py
+++ code/indexer.py
@@ -10,17 +10,35 @@ from nltk.corpus import stopwords # Stoppwortliste
 from nltk.stem import PorterStemmer # Stemmer
 
 #SQLAlchemy
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, select
 from sqlalchemy import MetaData, Table
 import config
-from models import Wordlist
+from models import Wordlist, Document
 
 # Encoding für die Datei
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
+#Verbindung herstellen, Tabellen laden
+engine = create_engine(config.DB_URI, echo=True)
+metadata = MetaData(bind=engine)
+wordtable = Table("wordlist", metadata, autoload=True)
+webpage = Table("document", metadata, autoload=True)
+
+# Nimm den Quelltext aus der Datenbank
+s = select([webpage.c.html_document]) 
+result = s.execute()
+ 
+# Jede Ergebniszeile (row) ist eine Liste mit einem Inhalt (item).
+# Momentan wird nur das letzte Ergebnis benutzt, weil die Schleife nicht mit dem Rest verbunden ist.
+# Nächste Aufgabe: Modularisierung!!!
+for row in result:
+	for item in row:
+		site = item
+
 # Öffnen der Datei/Seite
-soup = BeautifulSoup(open("index.html"))
+#soup = BeautifulSoup(open("index.html")) # Testversion
+soup = BeautifulSoup(site)
 
 # Entfernen der Kommentare
 for child in soup.body:
@@ -33,7 +51,7 @@ body_text = soup.body.getText()
 #Es müssen noch Satzzeichen und andere Störfaktoren entfert werden
 # Könnte man noch andersherum machen: If not in a.....z: replace
 # Man könnte für ./?/! einen end_of_sentence_indicator einbauen und so die Sätze zählen
-char_dict = {'?':'', '!':'', '-':'', ';':'', ':':'', '.':'', '...':'', '\n':' ', '/':'', '+':'', '<':'', '>':'', '}':'', '{':'', '=':'', ']':'', '[':''}
+char_dict = {'?':'', '!':'', '-':'', ';':'', ':':'', '.':'', '...':'', '\n':' ', '/':'', '+':'', '<':'', '>':'', '}':'', '{':'', '=':'', ']':'', '[':'', ')':'', '(':'', '|':''}
 for i, j in char_dict.iteritems():
     body_text = body_text.replace(i, j)
 
@@ -50,11 +68,6 @@ word_list = body_text.lower().split( )
 
 stop=stopwords.words('english')
 rdict={}
-
-#Verbindung herstellen, Tabelle laden
-engine = create_engine(config.DB_URI, echo=True)
-metadata = MetaData(bind=engine)
-wordtable = Table("wordlist", metadata, autoload=True)
 
 word_pos = 0
 stemmer=PorterStemmer()
@@ -76,19 +89,4 @@ for element in word_list:
 	except:
 		pass
 
-print rdict
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print rdict # Zum Testen
