4199637b9e6a996cee049fc92c749648600686ef
anmeh <anika.mehlem@hhu.de>
2015-06-25 13:34:36 +0200

    Commit nach allen 50 Websites, falls Programm abbricht

--- code/unusability.py
+++ code/unusability.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -
 import operator
 import urllib2
-
-
-
 import re
 import Queue
 import time
@@ -184,14 +181,12 @@ def get_distorted_images(url, soup,img_tags):
                 im=Image.open(urllib2.urlopen(url+tag.get("src")))
             except IOError:
                 pass
-                #print "Error opening image...",url+tag.get("src")
             except TypeError:
                 pass
             except urllib2.HTTPError:
                 pass
             except: #hier kommt ab und zu InvalidURL, aber der nimmt den ExceptionType nicht an..
                 pass
-                #print url+tag.get("src")
             else:   
                 try:
                     if round(float(im.size[0])/im.size[1],2) != round(float(tag.get("width"))/float(tag.get("height")),2):
@@ -223,20 +218,6 @@ def threads_images(soup):
     distorted_images = 1 if len(img_tags) != 0 and float(amount_distorted_images)/len(img_tags)>0.05 else 0
     return distorted_images
      
-def get_w3c_errors(all_urls,w3c_errors):
-    w3c_link = "https://validator.w3.org/check?uri="
-    for url in all_urls:
-        
-        check_url = urllib2.urlopen(w3c_link+url)
-        content = check_url.read()
-        soup = BeautifulSoup(content)
-        errors= soup.find("h3" ,class_ = "invalid")
-        if errors is not None:
-            errors_extracted = re.findall(r'\d+', str(errors.get_text()))
-            errors_extracted = [int(x) for x in errors_extracted]
-        else:
-            errors_extracted = [0]
-        w3c_errors[url] = errors_extracted[0]
 
 def get_w3c(url):
     w3c_link = "https://validator.w3.org/check?uri="
@@ -254,192 +235,78 @@ def get_w3c(url):
     return errors_extracted[0]
         
         
-def threads_w3c(all_urls):
-    thread_list = []
-    threads = 100
-    beg = 0
-    w3c_errors = {}
-    if len(all_urls)<threads:   
-        threads = len(all_urls)
-    end = len(all_urls)/threads
-    if all_urls:
-        while beg < len(all_urls):
-            t = Thread(target=get_w3c_errors, args=([all_urls[beg:end],w3c_errors]))
-            beg +=len(all_urls)/threads
-            end += len(all_urls)/threads
-            t.start()
-            thread_list.append(t)
-    for thread in thread_list:
-        thread.join()
-    return sorted(w3c_errors.items(), key=operator.itemgetter(0))
- 
-
 some_engine = create_engine(DB_URI, echo=DEBUG)
 Session = sessionmaker(bind=some_engine)    
 session = Session()
-websites = session.query(Document.html_document, Document.url).all()
+websites = session.query(Document.html_document, Document.url, Document.font_number).all()
 websites_data = []
 
 
-#Zeilen 272-328 = Alle Funktion + w3c mit Threads
-#########################################################################################################
-"""
-for url in websites:
-    all_urls.append(url[1])
-    
-overall_w3c_errors_of_all_sites =  threads_w3c(all_urls)
+website_counter = 0
+beg, end = -3, 0
+while end < len(websites):
+    beg += 3
+    end += 3
+    if end > len(websites):
+        end = len(websites)
+    for website in websites[beg:end]:
+        if website[2] != None:
+            website_counter += 1
+        else:
+            website_dict = {}
+            html = website[0].lower()
+            url = website[1]
+            soup = BeautifulSoup(html)
+            all_urls.append(url)
+            tags = get_tags(soup)
+            bad_fonts = get_bad_fonts(tags)
+            bad_colors = get_bad_colors(tags)
+            fonts = get_font_amount(tags)
+            marquee = get_html_textanimation(soup)
+            gifs = get_gifs(tags)
+            bad_structure =  get_site_structure(soup)
+            gb =  get_guestbook(html)
+            phrases = get_phrases(html)
+            dead_links = threads_links(soup)
+            audio = get_music(soup)[0]
+            audio_loop = get_music(soup)[1]
+            images = threads_images(soup)
+            flash = get_flash(soup)
+            popups = get_popups(soup)
+            counter = get_visitor_counter(soup)
+            w3 = get_w3c(url)
+               
+            factor = bad_fonts*4+bad_colors+fonts+gifs*0.5+marquee*2 
+            +bad_structure*2+gb*2+phrases+dead_links+audio*7 
+            +audio_loop*3+images*3+flash*2+popups*3+counter*2+w3
+            
+            website_dict['url'] = url
+            website_dict['font_existing'] = bad_fonts
+            website_dict['colour'] = bad_colors
+            website_dict['font_number'] = fonts
+            website_dict['textanimation'] = marquee
+            website_dict['number_of_gifs'] = gifs
+            website_dict['pagestructure'] = bad_structure
+            website_dict['guestbook'] =  gb
+            website_dict['phrases'] = phrases
+            website_dict['deadlinks'] = dead_links
+            website_dict['backgroundmusic'] = audio
+            website_dict['musicloop'] = audio_loop
+            website_dict['picture_distorted'] = images
+            website_dict['flash'] = flash
+            website_dict['popups'] = popups
+            website_dict['hitcounter'] = counter
+            website_dict['overall_score'] = factor
+            website_dict['w3c'] = w3
+            websites_data.append(website_dict)
+            website_counter+=1
+            print "Webseite %s von %s fertig" %(website_counter,len(websites))
 
-for website,w3c in zip(sorted(websites,key=operator.itemgetter(1)),overall_w3c_errors_of_all_sites):
-    website_dict = {}
-    html = website[0].lower()
-    url = website[1]
-    soup = BeautifulSoup(html)
-    all_urls.append(url)
-    tags = get_tags(soup)
-    bad_fonts = get_bad_fonts(tags)
-    bad_colors = get_bad_colors(tags)
-    fonts = get_font_amount(tags)
-    marquee = get_html_textanimation(soup)
-    gifs = get_gifs(tags)
-    bad_structure =  get_site_structure(soup)
-    gb =  get_guestbook(html)
-    phrases = get_phrases(html)
-    dead_links = threads_links(soup)
-    audio = get_music(soup)[0]
-    audio_loop = get_music(soup)[1]
-    images = threads_images(soup)
-    flash = get_flash(soup)
-    popups = get_popups(soup)
-    counter = get_visitor_counter(soup)
-    
-    
-    factor = bad_fonts*4+bad_colors+fonts+gifs*0.5+marquee*2 
-    +bad_structure*2+gb*2+phrases+dead_links+audio*7 
-    +audio_loop*3+images*3+flash*2+popups*3+counter*2+w3c[1]
-    
-    
-    website_dict['url'] = url
-    website_dict['font_existing'] = bad_fonts
-    website_dict['colour'] = bad_colors
-    website_dict['font_number'] = fonts
-    website_dict['textanimation'] = marquee
-    website_dict['number_of_gifs'] = gifs
-    website_dict['pagestructure'] = bad_structure
-    website_dict['guestbook'] =  gb
-    website_dict['phrases'] = phrases
-    website_dict['deadlinks'] = dead_links
-    website_dict['backgroundmusic'] = audio
-    website_dict['musicloop'] = audio_loop
-    website_dict['picture_distorted'] = images
-    website_dict['flash'] = flash
-    website_dict['popups'] = popups
-    website_dict['hitcounter'] = counter
-    website_dict['overall_score'] = factor
-    website_dict['w3c'] = w3c[1]
-    websites_data.append(website_dict)
-    print "rdy"
+    for website in websites_data:
+        for x in website.items():
+            update = session.query(Document).filter(Document.url == website.items()[5][1]).update({k:v for k,v in website.items()})
+            session.commit()
+        print "rdy"
+    websites_data = []
 
-"""
-##########################################################################################################
-
-
-#333-390 = Alles, aber w3c ohne Threads
-x = 0
-for website in websites:
-    website_dict = {}
-    html = website[0].lower()
-    url = website[1]
-    soup = BeautifulSoup(html)
-    all_urls.append(url)
-    tags = get_tags(soup)
-    bad_fonts = get_bad_fonts(tags)
-    bad_colors = get_bad_colors(tags)
-    fonts = get_font_amount(tags)
-    marquee = get_html_textanimation(soup)
-    gifs = get_gifs(tags)
-    bad_structure =  get_site_structure(soup)
-    gb =  get_guestbook(html)
-    phrases = get_phrases(html)
-    dead_links = threads_links(soup)
-    audio = get_music(soup)[0]
-    audio_loop = get_music(soup)[1]
-    images = threads_images(soup)
-    flash = get_flash(soup)
-    popups = get_popups(soup)
-    counter = get_visitor_counter(soup)
-    w3 = get_w3c(url)
-    
-    #factor = bad_fonts*4+bad_colors+fonts+gifs*0.5+marquee*2 
-    #+bad_structure*2+gb*2+phrases+dead_links+audio*7 
-    #+audio_loop*3+images*3+flash*2+popups*3+counter*2+w3c[1]
-    
-    factor = bad_fonts*4+bad_colors+fonts+gifs*0.5+marquee*2 
-    +bad_structure*2+gb*2+phrases+dead_links+audio*7 
-    +audio_loop*3+images*3+flash*2+popups*3+counter*2+w3
-    
-    website_dict['url'] = url
-    website_dict['font_existing'] = bad_fonts
-    website_dict['colour'] = bad_colors
-    website_dict['font_number'] = fonts
-    website_dict['textanimation'] = marquee
-    website_dict['number_of_gifs'] = gifs
-    website_dict['pagestructure'] = bad_structure
-    website_dict['guestbook'] =  gb
-    website_dict['phrases'] = phrases
-    website_dict['deadlinks'] = dead_links
-    website_dict['backgroundmusic'] = audio
-    website_dict['musicloop'] = audio_loop
-    website_dict['picture_distorted'] = images
-    website_dict['flash'] = flash
-    website_dict['popups'] = popups
-    website_dict['hitcounter'] = counter
-    website_dict['overall_score'] = factor
-    #website_dict['w3c'] = w3c[1]
-    website_dict['w3c'] = w3
-    websites_data.append(website_dict)
-    x+=1
-    print "Webseite %s von %s fertig" %(x,len(websites))
-
-
-
-##########################################################################################################
-for website in websites_data:
-    for x in website.items():
-  
-        update = session.query(Document).filter(Document.url == website.items()[5][1]).update({k:v for k,v in website.items()})
-        session.commit()
-    print "rdy"
-
-
-# Zum Testen
-# Läuft Datenbank durch, gibt alles aus
-"""
-for website in websites:
-    html = website[0].lower()
-    url = website[1]
-    all_urls.append(url)
-    soup = BeautifulSoup(html)
-    tags = get_tags(soup)
-   
-    #print url
-    #print "Schlechte Schriftarten ja/nein: ", get_bad_fonts(tags)
-    #print "Schlechte Farben: ", get_bad_colors(tags)
-    #print "Anzahl Schriften: ", font_amount(tags)
-    #print "Marquee: ", get_html_textanimation(soup)
-    #print "Gif Amount: ", get_gifs(tags)
-    #print "Bad Structure: ", bad_site_structure(soup)
-    #print "W3C Fehler: ", count_w3c_errors(url)
-    #print "Guestbook ja/nein:", get_guestbook(html)
-    #print "Schlechte Phrasen: ", get_phrases(html)
-    
-    #print "Background music?", music(soup)[0]
-    #print "Autoloop", music(soup)[1]
-    #print "Sind Bilder verzerrt?", threads_images(soup)
-    #print "Flash vorhanden?", get_flash(soup)
-    #print "Popups? ", get_popups(soup)
-    #print "Visitor Counter? ", visitor_counter(soup)
-    print "------------------------------"
-
-
-"""
 
