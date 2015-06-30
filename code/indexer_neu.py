b449f66d25794dbede2f06aa29abb2a232495460
ayilh <aylinilhan@Aylins-MacBook-Air.local>
2015-06-30 13:43:09 +0200

    Das ist die aktuellste Version!
    Allerdings funktioniert der IDF noch nicht ganz, da in der Wordlist
    Tabelle jedes Wort nur einmal vorkommen darf und jetzt der IDF über die
    ConsistsOf berechnet werden muss. Es muss also geschaut werden, wie oft
    eine WordID in der ConsistsOf vorkommt.

--- code/indexer_neu.py
+++ code/indexer_neu.py
@@ -10,6 +10,7 @@ from nltk.corpus import stopwords # Import Stopword list
 from nltk.stem import PorterStemmer # Import Stemmer
 
 #SQLAlchemy imports
+
 from sqlalchemy import create_engine, select
 from sqlalchemy import MetaData, Table
 import config
@@ -44,6 +45,10 @@ some_engine = create_engine(DB_URI, echo=DEBUG)
 Session = sessionmaker(bind=some_engine) 
 session = Session()
 
+
+
+
+
 def indexer(doc_id, sites):
 	soup = BeautifulSoup(sites)
 	for child in soup.body:
@@ -54,8 +59,7 @@ def indexer(doc_id, sites):
 
 	char_dict = {'?':'', '!':'', '-':'', ';':'', ':':'', '.':'', '...':'', '\n':' ', '/':'', '+':'', '<':'', '>':'', '}':'', '{':'', '=':'', ']':'', '[':'', ')':'', '(':'', '|':''}
 	for i, j in char_dict.iteritems():
-		body_text = body_text.replace(i, j)	
-	
+		body_text = body_text.replace(i, j)		
 	word_list = body_text.lower().split()
 
 
@@ -66,28 +70,33 @@ def indexer(doc_id, sites):
 	word_pos = 0 # Counter for word position
 	stemmer=PorterStemmer()
 	length_website = 0 # Counter for text length
-	N=118
-
-	#IDF=log(N/n)
-	#IDF_stoppword=log(N/n)+1
 	
+	dict_duplicate={}	
 	for element in word_list:
-		length_website=length_website+1		
-		try:
-			word_stem = stemmer.stem(element)
-			word_count = word_list.count(element) # Number of the word in this text
-			if element in stop:
-				is_stop = 1
-			else:
-				is_stop = 0
-			word_insert = wordtable.insert().execute(word=element, stem=word_stem, stopword=is_stop, number=word_count)
-			word_id = word_insert.inserted_primary_key
-			wdf_word = log(word_count)/log(2) +1 / log (length_website) /log(2)
-			#relation.insert().execute(document_documentid=Document.id, wordlist_wordid=Wordlist.id)
-			relation.insert().execute(document_documentid=doc_id, wordlist_wordid=word_id[0], wdf=wdf_word)
-			word_pos = word_pos+1
-		except:
+		length_website=length_website+1	
+		word_count = word_list.count(element)
+		if element not in dict_duplicate.keys():
+			dict_duplicate[element]=True
+			try:
+				word_stem = stemmer.stem(element)
+				 # Number of the word in this text
+				if element in stop:
+					is_stop = 1
+				else:
+					is_stop = 0
+				#word_insert = wordtable.insert().execute(word=element, stem=word_stem, stopword=is_stop, number=word_count)
+				#word_id = word_insert.inserted_primary_key
+				#wdf_word = log(word_count)/log(2) +1 / log (length_website) /log(2)
+				#
+				#relation.insert().execute(document_documentid=doc_id, wordlist_wordid=word_id[0], wdf=wdf_word)
+				word_pos = word_pos+1
+			except:
+				pass
+		else:
 			pass
+		# Number of the word in this text
+			#wdf_word = log(word_count)/log(2) +1 / log (length_website) /log(2)
+			#relation.insert().execute(document_documentid=doc_id, wordlist_wordid=word_id[0], wdf=wdf_word)
 
 
 if __name__ == "__main__":	
@@ -96,5 +105,15 @@ if __name__ == "__main__":
 	for element in html_document:
 		
 		indexer(element[0],element[1])
-	
-		
+
+
+N = session.query(Document).count()
+print N
+words=session.query(Wordlist.word).all()
+print words
+for element in words:
+	n = words.count(element)
+	print n
+	IDF = float(log(N/n))+1/log(2)
+#wordtable.update().execute(idf=IDF) 
+
