e0fdda0da3afcc173aab8febb03f1fda0aa304c8
M4urice <Maurice.Schleussinger@uni-duesseldorf.de>
2015-06-30 15:49:16 +0200

    Add detection for  javascript and the natural language to the indexer

--- code/indexer.py
+++ code/indexer.py
@@ -1,22 +1,27 @@
- -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 import sys
+import re
 
-#Die folgende Zeile führt unter Linux zu einer Fehlermeldung. Bitte ggf. (ent)kommentieren.
-from BeautifulSoup import BeautifulSoup
+# Die folgende Zeile führt unter Linux zu einer Fehlermeldung. Bitte ggf. (ent)kommentieren.
+#from BeautifulSoup import BeautifulSoup
 
-from bs4 import BeautifulSoup, Comment # Import BeautifulSoup und Comment
-from nltk.corpus import stopwords # Import Stopword list
-from nltk.stem import PorterStemmer # Import Stemmer
+from nltk.corpus import stopwords   # stopwords to detect language
+from nltk import wordpunct_tokenize  # function to split up our words
+from nltk.stem import PorterStemmer  # Import Stemmer
 
-#SQLAlchemy imports
+from bs4 import BeautifulSoup, Comment  # Import BeautifulSoup und Comment
+
+
+# SQLAlchemy imports
+
 from sqlalchemy import create_engine, select
 from sqlalchemy import MetaData, Table
 import config
 from math import log
-from sqlalchemy.ext.declarative import declarative_base 
-from sqlalchemy.orm import sessionmaker 
-from config import DB_URI, DEBUG 
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from config import DB_URI, DEBUG
 from models import Wordlist, Document, ConsistsOf
 #from models import wordlist, document
 #from database import db_session
@@ -26,7 +31,7 @@ reload(sys)
 sys.setdefaultencoding("utf-8")
 
 # Establish connection, load all necessary tables
-engine = create_engine(config.DB_URI, echo=True)
+engine = create_engine(config.DB_URI, echo=False)
 metadata = MetaData(bind=engine)
 wordtable = Table("wordlist", metadata, autoload=True)
 webpage = Table("document", metadata, autoload=True)
@@ -40,82 +45,117 @@ Base = declarative_base()
 #----------------------------------------------------------------------
 
 
-some_engine = create_engine(DB_URI, echo=DEBUG) 
-Session = sessionmaker(bind=some_engine) 
+some_engine = create_engine(DB_URI, echo=DEBUG)
+Session = sessionmaker(bind=some_engine)
 session = Session()
 
-<<<<<<< HEAD
-=======
+
+def get_language_likelihood(website):
+    """Return a dictionary of languages and their likelihood of being the
+    natural language of the input text
+    """
+
+    """ tokenize lower case web source """
+    website = website.lower()
+    words = wordpunct_tokenize(website)
+
+    candidates = {}
+    for language in stopwords._fileids:
+        candidates[language] = len(
+            set(words) & set(stopwords.words(language)))
+
+    return candidates
 
 
+def get_language(input_text):
+    """Return the most likely language of the given text
+    """
+
+    likelihoods = get_language_likelihood(input_text)
+
+    return sorted(likelihoods, key=likelihoods.get, reverse=True)[0]
 
 
->>>>>>> origin/master
-def indexer(doc_id, sites):
-	soup = BeautifulSoup(sites)
-	for child in soup.body:
-		if isinstance(child,Comment):
-			child.extract()
+def indexer(document):
+    if get_language(document.html_document) == "english":
+        soup = BeautifulSoup(document.html_document)
+        # remove javascript
+        for s in soup('script'):
+            s.extract()
+        # remove all comments
+        for child in soup.body:
+            if isinstance(child, Comment):
+                child.extract()
 
-	body_text = soup.body.getText()
+        body_text = soup.body.getText()
 
-	char_dict = {'?':'', '!':'', '-':'', ';':'', ':':'', '.':'', '...':'', '\n':' ', '/':'', '+':'', '<':'', '>':'', '}':'', '{':'', '=':'', ']':'', '[':'', ')':'', '(':'', '|':''}
-	for i, j in char_dict.iteritems():
-		body_text = body_text.replace(i, j)	
-	
-	word_list = body_text.lower().split()
+        char_dict = {'?': '', '!': '', '-': '', ';': '', ':': '', '.': '', '...': '', '\n': ' ', '/': '',
+                     '+': '', '<': '', '>': '', '}': '', '{': '', '=': '', ']': '', '[': '', ')': '', '(': '', '|': ''}
+        for i, j in char_dict.iteritems():
+            body_text = body_text.replace(i, j)
+        word_list = body_text.lower().split()
+
+        # save every word plus stem, count, etc. in the database/dictionary
+        # Use english stopword list from nltk corpus
+        stop = stopwords.words('english')
+
+        word_pos = 0  # Counter for word position
+        stemmer = PorterStemmer()
+        length_website = 0  # Counter for text length
+
+        dict_duplicate = {}
+        for element in word_list:
+            length_website = length_website + 1
+            word_count = word_list.count(element)
+            if element not in dict_duplicate.keys():
+                dict_duplicate[element] = True
+                try:
+                    word_stem = stemmer.stem(element)
+                    # Number of the word in this text
+                    if element in stop:
+                        is_stop = 1
+                    else:
+                        is_stop = 0
+                    word_insert = wordtable.insert().execute(
+                        word=element, stem=word_stem, stopword=is_stop, number=word_count)
+                    word_id = word_insert.inserted_primary_key
+                    wdf_word = log(word_count) / log(2) + 1 / \
+                        log(length_website) / log(2)
+                    #
+                    relation.insert().execute(
+                        document_documentid=document.id, wordlist_wordid=word_id[0], wdf=wdf_word)
+                    word_pos = word_pos + 1
+                except:
+                    pass
+            else:
+                wdf_word = log(word_count) / log(2) + 1 / \
+                    log(length_website) / log(2)
+                relation.update(
+                    document_documentid=document.id, wordlist_wordid=word_id[0], wdf=wdf_word)
 
 
-	
-	# save every word plus stem, count, etc. in the database/dictionary
-	stop = stopwords.words('english') # Use english stopword list from nltk corpus
+if __name__ == "__main__":
+    # get all documents from the db
+    html_document = session.query(
+        Document).yield_per(100)
 
-	word_pos = 0 # Counter for word position
-	stemmer=PorterStemmer()
-	length_website = 0 # Counter for text length
-<<<<<<< HEAD
-	N=118
+    # index all of them
+    for element in html_document:
+        indexer(element)
 
-	#IDF=log(N/n)
-	#IDF_stoppword=log(N/n)+1
-=======
-	
->>>>>>> origin/master
-	
-	for element in word_list:
-		length_website=length_website+1		
-		try:
-			word_stem = stemmer.stem(element)
-			word_count = word_list.count(element) # Number of the word in this text
-			if element in stop:
-				is_stop = 1
-			else:
-				is_stop = 0
-			word_insert = wordtable.insert().execute(word=element, stem=word_stem, stopword=is_stop, number=word_count)
-			word_id = word_insert.inserted_primary_key
-			wdf_word = log(word_count)/log(2) +1 / log (length_website) /log(2)
-			#relation.insert().execute(document_documentid=Document.id, wordlist_wordid=Wordlist.id)
-<<<<<<< HEAD
-			relation.insert().execute(document_documentid=doc_id, wordlist_wordid=word_id[0], wdf=wdf_word)
-=======
-			#relation.insert().execute(document_documentid=doc_id, wordlist_wordid=word_id[0], wdf=wdf_word)
->>>>>>> origin/master
-			word_pos = word_pos+1
-		except:
-			pass
+    # calculate idf
+    N = session.query(Document).count()
+    dictidf = {}
+    words = session.query(ConsistsOf.wordlist_wordid).all()
+    # print words
+    for element in words:
+        if element not in dictidf.keys():
+            dictidf[element] = 1
+        else:
+            dictidf[element] = dictidf[element] + 1
+    print dictidf
 
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/master
-if __name__ == "__main__":	
-	
-	html_document = session.query(Document.id, Document.html_document).all()
-	for element in html_document:
-		
-		indexer(element[0],element[1])
-<<<<<<< HEAD
-	
-		
-=======
->>>>>>> origin/master
+    # for values in dictidf.values():
+    #   IDF = float(log(N/values))+1/log(2)
+    # print IDF
+# wordtable.update().execute(idf=IDF)
